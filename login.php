da7d4637dcc4770c263ff2e84dfe59e8dfe24a3b
AlexCLien <alexclien@gmail.com>
2018-12-06 11:22:58 -0800

    add login.php

---
+++ login.php
@@ -0,0 +1,42 @@
+<?php
+    $host = "127.0.0.1";
+    $user = "lmillard";
+    $pass = "";
+   $db = "sample_db";
+    $port = 3306;
+ ?>
+ 
+formtest.html
+<html>
+<head>
+	<title>phptest</title>
+</head>
+<body>
+	<form action = 'phptest2.php' method = "post">
+		<select name = "myTest">
+			<option value = "username">User Name</option>
+			<option value = "email">email</option>
+		</select>
+		<input type = "submit" value="submit the form">
+	</form>
+</body>
+</html>
+
+phptest2.php
+	<?php
+		include_once 'formtest.html';
+		require_once 'login.php';
+		$conn = mysqli_connect($host, $user, $pass, $db, $port);
+		if($conn->connect_error) die($conn->connect_error);
+		$option = isset($_POST['myTest']) ? $_POST['myTest'] : false;
+		if ($option){
+			$testField = $_POST['myTest'];
+			$query = "	select ". $testField . " from users;";
+		$result = mysqli_query($conn, $query);
+			while ($row = mysqli_fetch_assoc($result)) {
+				echo $row[$testField] . "<br>";
+			}
+		}
+		$result->close();
+		$conn->close();
+	?>
